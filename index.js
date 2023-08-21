6623732e2ffad244a6cfee7f55b7542fbc2dcdfc
tantality <angel.tretyak@mail.ru>
2023-08-21 19:26:03 +0300

    feat: add a proxy with cors

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hello');
+const express = require("express");
+const proxy = require("express-http-proxy");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors({origin: true}));
+
+app.use(
+  "/",
+  proxy(process.env.ORIGINAL_SERVER_URL, { https: true })
+);
+
+app.use((err, _req, res, _next) => {
+  console.error(err.stack);
+  res.status(500).send("Server error");
+});
+
+app.listen(port, () => {
+  console.log("Server started");
+});
