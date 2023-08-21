2f0d92a4e82d8d52e52a5de7d3648c5a51a2b38d
tantality <angel.tretyak@mail.ru>
2023-08-21 22:15:51 +0300

    feat: implement attaching cookies saved between requests to the response

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require("express");
 const proxy = require("express-http-proxy");
 const cors = require("cors");
+const requestIp = require("request-ip");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
+const cookiePerIP = new Map();
 
-app.use(cors({origin: true}));
+app.use(requestIp.mw());
+
+app.use(cors({ origin: true }));
 
 app.use(
   "/",
-  proxy(process.env.ORIGINAL_SERVER_URL, { https: true })
+  proxy(process.env.ORIGINAL_SERVER_URL, {
+    https: true,
+    userResHeaderDecorator(headers, userReq, userRes, proxyReq, proxyRes) {
+      const key = userReq.clientIp;
+
+      if (headers["set-cookie"]) {
+        const newCookies = headers["set-cookie"].map((cookie) => {
+          const [key, value] = cookie.split(";")[0].split("=");
+          return { key, value };
+        });
+
+        const previousCookies = cookiePerIP.get(key) || [];
+        const currentCookies = previousCookies.concat(newCookies);
+
+        cookiePerIP.set(key, currentCookies);
+      }
+
+      return headers;
+    },
+  })
 );
 
 app.use((err, _req, res, _next) => {
